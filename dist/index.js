998bc504fbc44679353ea5665b65e53be385827c
Giorgi Kheladze <giorgikheladze546@gmail.com>
2024-11-24 19:13:00 +0400

    update index.ts

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,7 @@ let sales = 123456789;
 let giorgi = "giorgi";
 let bool = true;
 let level;
+level = 10;
+level = "a";
+console.log(level);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
+// types:
+//  build-in
+//  any
+//  never
+//  emun
+//  tuple
+
 // buid-in-variable-types
 let sales: number = 123_456_789;
 let giorgi: string = "giorgi";
 let bool: boolean = true;
 let level;
+level = 10;
+level = "a";
+console.log(level)
+
+//type any 
