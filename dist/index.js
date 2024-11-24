fa23c39c37a6e219b09da2a022d8c78df7493abb
Giorgi Kheladze <157839203+giorgi-kheladze@users.noreply.github.com>
2024-11-24 18:46:07 +0400

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let age = 19;
+if (age < 50) {
+    age += 10;
+    console.log(age);
+    age += 12;
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let age: number = 19;
+if(age < 50){
+    age += 10
+    console.log(age)
+    age += 12
+}
