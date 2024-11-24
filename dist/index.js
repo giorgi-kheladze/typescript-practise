67f90180e8fec5b2505b59a8152128d08c2d0570
Giorgi Kheladze <giorgikheladze546@gmail.com>
2024-11-24 19:35:57 +0400

    new update

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,11 @@ let level;
 level = 10;
 level = "a";
 console.log(level);
+function render(document) {
+    console.log(document);
+}
+render("giorg");
+let array = [10, "giorgi"];
+let arr1 = array[0];
+console.log(arr1);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,12 @@ level = "a";
 console.log(level)
 
 //type any 
+function render(document: string){
+    console.log(document);
+}
+render("giorg")
+//arrays
+
+let array: [any, any] = [10, "giorgi"]
+let arr1 = array[0];
+console.log(arr1)
